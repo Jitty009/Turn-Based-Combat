2046a38564ddefac6a6af9948883b01dce3ea7ad
Jitty009 <clappingmao@gmail.com>
2016-10-15 11:15:13 -0500

    Update TurnOrder.java

--- TurnOrder.java
+++ TurnOrder.java
@@ -15,8 +15,36 @@ public class TurnOrder {
     static int turnCounter = 0;
     static int turnCounterDisplay = 1;
     public static ArrayList<Integer> turnOrder = new ArrayList();
+    static ArrayList<Character> characterList = new ArrayList();
+    static ArrayList<Integer> initiativeOrder = new ArrayList();
+    
+    private void addCharacterList(Character target) {
+        characterList.add(target);   
+    }
+    
+    private void addInitiativeOrder() {
+        for (int i = 0 ; i < characterList.size() ; i++) {
+            initiativeOrder.add(characterList[i].getInitiative());
+        }
+    }
+    
+    private void sortInitiativeOrder() {
+        for (int i = 1 ; i < initiativeOrder.size() ; i++) {
+            for (int j = i ; initiativeOrder[j] > initiativeOrder[(j - 1)] ; j--) {
+                Collections.swap(initiativeOrder, j, j-1);
+            }
+        }
+    }
+    
+    void establishInitiative(Character... target) {
+        addCharacterList(target);
+        addInitiativeOrder;
+        sortInitiativeOrder();
+    }
     
     void addTurnOrder(Character target) {
+        //When the Initiative ArrayList is sorted greatest to least, this should iterate through it and add the ID Numbers of its elements
+        // to the turnOrder ArrayList.
         turnOrder.add(target.getIdNumber());
     }
 }
